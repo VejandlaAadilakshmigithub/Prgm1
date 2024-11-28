c9d5599f738977aae57b7301f1916fe81d05e30f
VejandlaAadilakshmigithub <vejandlalakshmi2000@gmail.com>
2024-11-28 14:48:04 +0530

    HMI Skin Theme Example

--- Prgm1.cpp
+++ Prgm1.cpp
@@ -1,5 +1,5 @@
 
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 #include <string>
 
@@ -146,4 +146,77 @@ int main() {
     handleUserInput(navigator);
 
     return 0;
+}
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+struct menuNode{
+    string data ;
+    vector<menuNode*> child;
+ 
+    menuNode(string data){
+        this->data = data;
+    }
+    void addchild(menuNode *data){
+        child.push_back(data);
+    }  
+};
+ 
+ 
+void displayMenu(menuNode *menu){
+     cout<<" == "<<menu->data<<" == "<<endl;
+     if(menu->data.empty()){
+        cout<<"No sub menu available...."<<endl;
+     }else{
+        for(auto  i = 0 ; i < menu->child.size(); i++){
+            cout<<i+1 <<". "<<menu->child[i]->data<<endl;
+        }
+     }  
+}
+void navigate(menuNode *menu){
+   int choice ;
+     while(true){
+           displayMenu(menu);
+           cout<<"Choose an option(enter 0 to go back): "<<endl;
+           cin>>choice;
+ 
+           if(choice == 0){
+            cout<<"Going back... "<<endl;
+            break;
+           }
+           
+        if(choice <1 || choice > menu->child.size()){
+            cout<<"Invalid choice..."<<endl;
+        }else{
+            cout << "Navigating to " << menu->child[choice-1]->data << "...\n";
+            navigate(menu->child[choice]);
+        }                
+     }
+}
+int main(){
+    menuNode *root = new menuNode("Main menu");
+    menuNode *submenu1 = new menuNode("settings");
+    menuNode *submenu2 = new menuNode("media");
+ 
+    root->addchild(submenu1);
+    root->addchild(submenu2);
+     
+    menuNode *submenu1_1 = new menuNode("Display settings");
+    menuNode *submenu1_2 = new menuNode("Audio settings");
+ 
+    submenu1->addchild(submenu1_1);
+    submenu1->addchild(submenu1_2);
+ 
+    menuNode *submenu2_1 = new menuNode("Radio");
+    menuNode *submenu2_2 = new menuNode("Bluethoo");
+ 
+    //displayMenu(root);
+    cout<<"Welcome to our playlist...."<<endl;
+ 
+    navigate(root);
+ 
+   return 0;
 }
